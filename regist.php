32eaf241562d76999d1ce0f0968d3d7176f18b97
adam <0_markad_0@mail.ru>
2022-03-06 01:03:02 +0300

    registrtion

---
+++ regist.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Регистрация.</h1>
+    <form method="POST" action="do_regist.php">
+        <p><b>Ваш логин:</b><br>
+        <input name="user"/> <br />
+        <p><b>Ваш пароль:</b><br>
+        <input type="password" name="pwd"/> <br />
+        <button>Далее</button>
+    </form>
+    
+</body>
+</html>
