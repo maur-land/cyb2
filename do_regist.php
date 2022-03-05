dd6bba144b2a1ad210b6bb3c35dcc637ac21d712
adam <0_markad_0@mail.ru>
2022-03-06 01:02:30 +0300

    registration

---
+++ do_regist.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check login</title>
+</head>
+<body>
+    <?php
+        $user = $_REQUEST['user'];
+        $hash = hash("sha256", $_REQUEST['pwd']);
+
+        include("../../../params/billing.php");
+        $conn = mysqli_connect($db_server,$db_user,$db_pwd,"billing");
+        $sql = "INSERT INTO users(Login, Pwdhash) VALUES('$user', '$hash')";
+        mysqli_query($conn, $sql);
+        mysqli_close($conn);
+    ?>
+</body>
+</html>
